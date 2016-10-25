87f31f6c65b0f37fa35c7ef7fe9674fd76eccead
Eric Freudenthal <efreudenthal@utep.edu>
2016-10-25 15:49:57 -0600

    fixed ball-demo to use col/row

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 all:
 	(cd timerLib; make install)
+	(cd lcdLib; make install)
 	(cd ball_demo; make)
 
 clean:
 	(cd timerLib; make clean)
+	(cd lcdLib; make clean)
 	(cd ball_demo; make clean)
 	rm -rf lib h
--- ball_demo/main.c
+++ ball_demo/main.c
@@ -11,8 +11,8 @@ u_char LOWER_BOUNDARY, UPPER_BOUNDARY, LEFT_BOUNDARY, RIGHT_BOUNDARY; /* Boundar
 u_char width, height;       /* screen width and height */
 u_char chords14[RADIUS+1];	/* for a circle of radius 14 */
 
-u_char px, py;        /* position coordinates of ball */
-signed int deltaX = 1, deltaY = 1;
+u_char pcol, prow;        /* position coordinates of ball */
+signed int deltaCOL = 1, deltaROW = 1;
 u_char count = 0;    
 
 void drawArena()
@@ -20,14 +20,14 @@ void drawArena()
   drawRectOutline(LEFT_BOUNDARY, LOWER_BOUNDARY, ARENA_WIDTH, ARENA_HEIGHT, COLOR_BLACK);
 }
 
-fillCircle(u_char x, u_char y, u_char radius, u_char chords[], u_int color)
+fillCircle(u_char col, u_char row, u_char radius, u_char chords[], u_int color)
 {
-  u_char dy;
-  for (dy = 0; dy < radius; dy++) {
-    u_char dx = chords[dy];
-    u_char width = 1 + (dx << 1);
-    fillRectangle(x-dx, y+dy, width, 1, color);
-    fillRectangle(x-dx, y-dy, width, 1, color);
+  u_char drow;
+  for (drow = 0; drow < radius; drow++) {
+    u_char dcol = chords[drow];
+    u_char width = 1 + (dcol << 1);
+    fillRectangle(col-dcol, row+drow, width, 1, color);
+    fillRectangle(col-dcol, row-drow, width, 1, color);
   }
 }
 
@@ -44,14 +44,14 @@ main()
   LEFT_BOUNDARY = (width>>1) - width_offset;
   RIGHT_BOUNDARY = (width>>1) + width_offset;
   
-  px = (width>>1);
-  py = (height>>1);
+  pcol = (width>>1);
+  prow = (height>>1);
   
   clearScreen(COLOR_BLUE);
   drawString5x7((width>>1) - 20, 20, "Arena", COLOR_GREEN, COLOR_RED);
   
   drawArena();
-  fillCircle(px, py, RADIUS, chords14, COLOR_RED);
+  fillCircle(pcol, prow, RADIUS, chords14, COLOR_RED);
   computeChordVec(chords14, 14);
   
   or_sr(0x18);  /* CPU off, GIE on */
@@ -59,26 +59,26 @@ main()
 
 void update_ball() 
 {
-  u_char px_new = px + deltaX;
-  u_char py_new = py + deltaY;
+  u_char pcol_new = pcol + deltaCOL;
+  u_char prow_new = prow + deltaROW;
   
-  if (px_new - RADIUS <= LEFT_BOUNDARY || (px_new + RADIUS >= RIGHT_BOUNDARY))
-    deltaX = -deltaX;
+  if (pcol_new - RADIUS <= LEFT_BOUNDARY || (pcol_new + RADIUS >= RIGHT_BOUNDARY))
+    deltaCOL = -deltaCOL;
   
-  if (py_new - RADIUS <= LOWER_BOUNDARY || py_new + RADIUS >= UPPER_BOUNDARY)
-    deltaY = -deltaY;
+  if (prow_new - RADIUS <= LOWER_BOUNDARY || prow_new + RADIUS >= UPPER_BOUNDARY)
+    deltaROW = -deltaROW;
   
-  px = px_new + deltaX;
-  py = py_new + deltaY;
+  pcol = pcol_new + deltaCOL;
+  prow = prow_new + deltaROW;
 }
 
 __interrupt(WDT_VECTOR) WDT()
 {  
   drawArena();
   if (++count == 10) {
-    fillCircle(px, py, RADIUS, chords14, COLOR_BLUE);
+    fillCircle(pcol, prow, RADIUS, chords14, COLOR_BLUE);
     update_ball();
-    fillCircle(px, py, RADIUS, chords14, COLOR_RED);
+    fillCircle(pcol, prow, RADIUS, chords14, COLOR_RED);
     count = 0;
   }
 }
