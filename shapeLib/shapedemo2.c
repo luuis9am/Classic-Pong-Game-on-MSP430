f9bd29149a49c4a1c564af9ffb6d808f7d65ee48
Eric Freudenthal <efreudenthal@utep.edu>
2016-10-28 23:44:16 -0600

    removed detritus left over from timkering

--- shapeLib/shapedemo2.c
+++ shapeLib/shapedemo2.c
@@ -17,28 +17,8 @@ void makeCircle14()
 
 AbRect rect10 = {abRectGetBounds, abRectCheck, 10,10};;
 
-abDrawPos(AbShape *shape, Vec2 *shapeCenter, u_int fg_color, u_int bg_color)
-{
-  u_char row, col;
-  Region bounds;
-  abShapeGetBounds(shape, shapeCenter, &bounds);
-  lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1],
-	      bounds.botRight.axes[0]-1, bounds.botRight.axes[1]-1);
-  for (row = bounds.topLeft.axes[1]; row < bounds.botRight.axes[1]; row++) {
-    for (col = bounds.topLeft.axes[0]; col < bounds.botRight.axes[0]; col++) {
-      Vec2 pixelPos = {col, row};
-      int color = abShapeCheck(shape, shapeCenter, &pixelPos) ?
-	fg_color : bg_color;
-      lcd_writeColor(color);
-    }
-  }
-}
-
-
-
 
 Region fence = {{10,30}, {SHORT_EDGE_PIXELS-10, LONG_EDGE_PIXELS-10}};
-
 
 
 #define numLayers 2
