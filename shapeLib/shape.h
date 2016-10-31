cd4b369ebce80cab22d42e18564d70396ca6a9a9
dgnajera <dgnajera2@miners.utep.edu>
2016-10-30 21:25:55 -0600

    Updated doxygen documentation

--- shapeLib/shape.h
+++ shapeLib/shape.h
@@ -7,8 +7,10 @@
 
 #include "lcdutils.h"
 
-/** Vector structure that contains:
- *  an array of two integers 
+/** Vector structure containing:
+ *  an array of two integers that can be used
+ *  to describe a position (x,y), or dimensions
+ *  such as length and width.
  */
 typedef struct {
   int axes[2];
@@ -18,40 +20,39 @@ extern Vec2 screenSize, screenCenter, vec2Unit, vec2Zero;
 
 /** Gives the max vector between v1 and v2
  *  
- *  \param vecMax The max vector
- *  \param v1 Vector 1
- *  \param v2 Vector 2
+ *  \param vecMax (out) The max vector
+ *  \param (in) v1 Vector 1
+ *  \param (in) v2 Vector 2
  */ 
 void vec2Max(Vec2 *vecMax, Vec2 *v1, Vec2 *v2);
 
 /** Gives the min vector between v1 and v2
  *  
- *  \param vecMin The min vector
- *  \param v1 Vector 1
- *  \param v2 Vector 2
+ *  \param vecMin (out) The min vector
+ *  \param v1 (in) Vector 1
+ *  \param v2 (in) Vector 2
  */ 
 void vec2Min(Vec2 *vecMin, Vec2 *v1, Vec2 *v2);
 
 /** Gives the summed vector of v1 and v2 (v1 + v2)
  *  
- *  \param result The resulting vector of v1 + v2
- *  \param v1 Vector 1
- *  \param v2 Vector 2
+ *  \param result (out) The resulting vector of v1 + v2
+ *  \param v1 (in) Vector 1
+ *  \param v2 (in) Vector 2
  */ 
 void vec2Add(Vec2 *result, Vec2 *a1, Vec2 *a2);
 
 /** Gives the subtracted vector between v1 and v2 (v1 - v2)
  *  
- *  \param result The resulting vector of v1 - v2
- *  \param v1 Vector 1
- *  \param v2 Vector 2
+ *  \param result (out) The resulting vector of v1 - v2
+ *  \param v1 (in) Vector 1
+ *  \param v2 (in) Vector 2
  */ 
 void vec2Sub(Vec2 *result, Vec2 *a1, Vec2 *a2);
 
 /** Gives the absolute value of vector
  *  
- *  \param vecMax The max vector
- *  \param vec The vector
+ *  \param vec (in and out) The vector
  */ 
 void vec2Abs(Vec2 *vec);
 
@@ -66,15 +67,15 @@ typedef struct {
 
 /** This function computes the union of two regions
  *  
- *  \param rUnion The unioned region of r1 and r2
- *  \param r1 The first region
- *  \param r2 The second region
+ *  \param rUnion (out) The unioned region of r1 and r2
+ *  \param r1 (in) The first region
+ *  \param r2 (in) The second region
  */
 void regionUnion(Region *rUnion, Region *r1, Region *r2);
 
 /** Trims extend of region to screen bounds 
  *  
- *  \param region The region
+ *  \param region (in and out) The region
  */
 void regionClipScreen(Region *region);
 
@@ -94,17 +95,17 @@ typedef struct AbShape_s{		/* base type for all abstrct shapes */
 
 /** Computes bounding box of abShape in screen coordinates 
  *
- *  \param s The abstract shape
- *  \param centerPos The Vec2 specifying the center poisiton of the shape
- *  \param bounds The computed bounding box region
+ *  \param s (in) The abstract shape
+ *  \param centerPos (in) The Vec2 specifying the center poisiton of the shape
+ *  \param bounds (out) The computed bounding box region
  */
 void abShapeGetBounds(AbShape *s, Vec2 *centerPos, Region *bounds);
 
 /** Check if pixel is within the abShape centered at centerPos
  *
- *  \param shape The abstract shape
- *  \centerPos The Vec2 specifying the center position of the shape
- *  \pixelLoc The Vec2 specifying the location of the pixel
+ *  \param shape (in) The abstract shape
+ *  \param centerPos (in) The Vec2 specifying the center position of the shape
+ *  \param pixelLoc (in) The Vec2 specifying the location of the pixel
  *  \return True (1) if pixel is in the abShape centered at centerPos 
  */
 int abShapeCheck(AbShape *shape, Vec2 *centerPos, Vec2 *pixelLoc);
@@ -123,17 +124,17 @@ typedef struct AbRArrow_s {
 /** Computes bounding box in screen coordinates for arrow with 
  *  whose tip is at centerPos
  *  
- *  \param arrow The abstract arrow
- *  \param centerPos The Vec2 specifying the center position of the arrow
- *  \param The computed bounded box region
+ *  \param arrow (in) The abstract arrow
+ *  \param centerPos (in) The Vec2 specifying the center position of the arrow
+ *  \param bounds (out) The computed bounded box region
  */
 void abRArrowGetBounds(AbRArrow *arrow, Vec2 *centerPos, Region *bounds);
 
 /** Check if pixel is within the abArrow centered at centerPos
  *
- *  \param arrow The abstract arrow
- *  \param centerPos The Vec2 specifying the center position of the arrow
- *  \param pixel The Vec2 specifying the location of the pixel
+ *  \param arrow (in) The abstract arrow
+ *  \param centerPos (in) The Vec2 specifying the center position of the arrow
+ *  \param pixel (in) The Vec2 specifying the location of the pixel
  *  \return True (1) if pixel is in arrow whose tip is at centerPos
  */
 int abRArrowCheck(AbRArrow *arrow, Vec2 *centerPos, Vec2 *pixel);
@@ -151,17 +152,17 @@ typedef struct AbRect_s {
 
 /** Computes bounding box in screen coordinates for rect centered at centerPos
  *
- *  \param rect The abstract rectangle
- *  \param centerPos The Vec2 specifying the center position of the rectangle
- *  \param bounds The computed bounded box region
+ *  \param rect (in) The abstract rectangle
+ *  \param centerPos (in) The Vec2 specifying the center position of the rectangle
+ *  \param bounds (out) The computed bounded box region
  */
 void abRectGetBounds(AbRect *rect, Vec2 *centerPos, Region *bounds);
 
 /** Check if pixel is within the abRect centered at centerPos
  *
- *  \param rect The abstract rectangle
- *  \param centerPos The Vec2 specifying the center position of the rectangle
- *  \param pixel The Vec2 specifying the location of the pixel
+ *  \param rect (in) The abstract rectangle
+ *  \param centerPos (in) The Vec2 specifying the center position of the rectangle
+ *  \param pixel (in) The Vec2 specifying the location of the pixel
  *  \return True (1) if pixel is in rectangle whose tip is at centerPos
  */
 int abRectCheck(AbRect *rect, Vec2 *centerPos, Vec2 *pixel);
@@ -181,16 +182,16 @@ typedef struct AbCircle_s {
 
 /** Computes bounding box in screen coordinates for circle centered at circlePos
  *
- *  \param circle The abstract circle
- *  \param circlePos The Vec2 specifying the center position of the circle
- *  \param bounds The computed bounded box region
+ *  \param circle (in) The abstract circle
+ *  \param circlePos (in) The Vec2 specifying the center position of the circle
+ *  \param bounds (out) The computed bounded box region
  */void abCircleGetBounds(AbCircle *circle, Vec2 *circlePos, Region *bounds);
 
 /** Check if pixel is within the abRect centered at centerPos
  *
- *  \param circle The abstract circle
- *  \param circlePos The Vec2 specifying the center position of the circle
- *  \param pixel The Vec2 specifying the location of the pixel
+ *  \param circle (in) The abstract circle
+ *  \param circlePos (in) The Vec2 specifying the center position of the circle
+ *  \param pixel (in) The Vec2 specifying the location of the pixel
  *  \return True (1) if pixel is in circle whose tip is at circlePos
  */int abCircleCheck(AbCircle *circle, Vec2 *circlePos, Vec2 *pixel);
 
