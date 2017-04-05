d9bd4bc8d0640cc0b2302798a21dee1c189dda4c
robustUTEP <efreudenthal@utep.edu>
2017-04-05 03:00:27 -0600

    removed dead code

--- shapeLib/layer.c
+++ shapeLib/layer.c
@@ -42,32 +42,3 @@ layerInit(Layer *layer)
     layer->posLast = layer->posNext = layer->pos;
 }
 
-/* void */
-/* layerDrawOld(Layer *layers) */
-/* { */
-/*   Layer *boundLayer, *probeLayer; */
-/*   for (boundLayer=layers; boundLayer; boundLayer=boundLayer->next) { */
-/*     int row, col; */
-/*     Region bounds; */
-/*     layerGetBounds(boundLayer, &bounds); */
-/*     lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1], */
-/* 		bounds.botRight.axes[0], bounds.botRight.axes[1]); */
-/*     for (row = bounds.topLeft.axes[1]; row <= bounds.botRight.axes[1]; row++) { */
-/*       for (col = bounds.topLeft.axes[0]; col <= bounds.botRight.axes[0]; col++) { */
-/* 	Vec2 pixelPos = {col, row}; */
-/* 	u_int color = bgColor; */
-/* 	for (probeLayer = layers; probeLayer; probeLayer = probeLayer->next) { */
-/* 	  if (abShapeCheck(probeLayer->abShape, &probeLayer->pos, &pixelPos)) { */
-/* 	    color = probeLayer->color; */
-/* 	    break;  */
-/* 	  } /\* if check *\/ */
-/* 	} // for checking all layers at col, row */
-/* 	lcd_writeColor(color);  */
-/*       } // for col */
-/*     } // for row */
-/*   } // for layer being rendered */
-/*   for (boundLayer=layers; boundLayer; boundLayer=boundLayer->next) */
-/*     boundLayer->dispPos = boundLayer->pos; */
-/* }	   */
-
-
