f65a0cf322d5cf9ad33b5ebb83c835bf07a303e8
Luis A Mireles <luui9mirel@gmail.com>
2017-12-01 15:20:57 -0700

    import buzzer file
    buzzer file is from project 2 demo "buzzer_demo.c"

---
+++ pong/buzzer.c
@@ -0,0 +1,28 @@
+#include <msp430.h>
+#include "libTimer.h"
+#include "buzzer.h"
+
+void buzzer_init()
+{
+    /* 
+       Direct timer A output "TA0.1" to P2.6.  
+        According to table 21 from data sheet:
+          P2SEL2.6, P2SEL2.7, anmd P2SEL.7 must be zero
+          P2SEL.6 must be 1
+        Also: P2.6 direction must be output
+    */
+    timerAUpmode();		/* used to drive speaker */
+    P2SEL2 &= ~(BIT6 | BIT7);
+    P2SEL &= ~BIT7; 
+    P2SEL |= BIT6;
+    P2DIR = BIT6;		/* enable output to speaker (P2.6) */
+
+    buzzer_set_period(1000);	/* start buzzing!!! */
+}
+
+void buzzer_set_period(short cycles)
+{
+  CCR0 = cycles; 
+  CCR1 = cycles >> 1;		/* one half cycle */
+}
+
