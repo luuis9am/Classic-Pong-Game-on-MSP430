9ac42cb4db0c50227db643307b324298f6beac36
luis9am <luui9mirel@gmail.com>
2017-12-01 17:28:26 -0700

    build

--- pong/pong.c
+++ pong/pong.c
@@ -1,1 +1,103 @@
 //pong game
+
+#include <msp430.h>
+#include <libTimer.h>
+#include <lcdutils.h>
+#include <lcddraw.h>
+#include <shape.h>
+#include <abCircle.h>
+#include "buzzer.h"
+
+#define GREEN_LED BIT6
+
+#include "p2switches.h"
+
+
+
+
+AbRect paddle = {abRectGetBounds, abRectCheck, {15,3}}; // 15x3 rectangle
+ABRect halfCourt = {abRectGetBounds, abRectCheck, {61, 0}}; //horizontal court line
+
+abRectOutline fieldOutline = {
+  abRectOutlineGetBounds, abRectOutlineCheck,
+  {screenWidth/2 - 2, screenHeight/2 -6}
+};
+
+Layer fieldLayer = {
+  (AbShape *) &fieldOutline,
+  {screenWidth/2, screenHeight/2 -3}, //centered
+  {0,0}, {0,0}, //last & next pos
+  COLOR_WHITE,
+  0
+};
+
+
+
+
+
+//Function movLayerDraw() & m1Advance() implemented from Lab3 "shape-motion_demo" shapemotion.c
+
+void movLayerDraw(MovLayer *movLayers, Layer *layers)
+{
+  int row, col;
+  MovLayer *movLayer;
+
+  and_sr(~8);			/**< disable interrupts (GIE off) */
+  for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
+    Layer *l = movLayer->layer;
+    l->posLast = l->pos;
+    l->pos = l->posNext;
+  }
+  or_sr(8);			/**< disable interrupts (GIE on) */
+
+
+  for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
+    Region bounds;
+    layerGetBounds(movLayer->layer, &bounds);
+    lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1], 
+		bounds.botRight.axes[0], bounds.botRight.axes[1]);
+    for (row = bounds.topLeft.axes[1]; row <= bounds.botRight.axes[1]; row++) {
+      for (col = bounds.topLeft.axes[0]; col <= bounds.botRight.axes[0]; col++) {
+	Vec2 pixelPos = {col, row};
+	u_int color = bgColor;
+	Layer *probeLayer;
+	for (probeLayer = layers; probeLayer; 
+	     probeLayer = probeLayer->next) { /* probe all layers, in order */
+	  if (abShapeCheck(probeLayer->abShape, &probeLayer->pos, &pixelPos)) {
+	    color = probeLayer->color;
+	    break; 
+	  } /* if probe check */
+	} // for checking all layers at col, row
+	lcd_writeColor(color); 
+      } // for col
+    } // for row
+  } // for moving layer being updated
+}	  
+
+
+Region fence = {{10,30}, {SHORT_EDGE_PIXELS-10, LONG_EDGE_PIXELS-10}}; /**< Create a fence region */
+
+
+/** Advances a moving shape within a fence
+ *  
+ *  \param ml The moving shape to be advanced
+ *  \param fence The region which will serve as a boundary for ml
+ */
+void mlAdvance(MovLayer *ml, Region *fence)
+{
+  Vec2 newPos;
+  u_char axis;
+  Region shapeBoundary;
+  for (; ml; ml = ml->next) {
+    vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
+    abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
+    for (axis = 0; axis < 2; axis ++) {
+      if ((shapeBoundary.topLeft.axes[axis] < fence->topLeft.axes[axis]) ||
+	  (shapeBoundary.botRight.axes[axis] > fence->botRight.axes[axis]) ) {
+	int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
+	newPos.axes[axis] += (2*velocity);
+      }	/**< if outside of fence */
+    } /**< for axis */
+    ml->layer->posNext = newPos;
+  } /**< for ml */
+}
