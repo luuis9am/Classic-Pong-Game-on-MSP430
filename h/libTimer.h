2ed783d1a53e5be8e064f82217268235cf030bc7
luis9am <luui9mirel@gmail.com>
2017-12-04 00:15:30 -0700

    editing

--- h/libTimer.h
+++
@@ -1,7 +0,0 @@
-#ifndef libTimer_included
-#define libTimer_included
-
-#include "clocksTimer.h"
-#include "sr.h"
-
-#endif // included
--- h/p2switches.h
+++
@@ -1,9 +0,0 @@
-#ifndef switches_included
-#define switches_included
-
-#include "msp430.h"
-
-unsigned int p2sw_read();
-void p2sw_init(unsigned char mask);
-
-#endif // included
